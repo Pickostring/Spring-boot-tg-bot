73dafdd0507bc2bdc9ef2c58e8e48cf9198b2dd6
Dmitry Rovinsky <ecco7391@gmail.com>
2022-09-28 00:02:11 +0500

    Hide bot-token from code

--- src/main/java/home/picko/springdemobot/config/BotConfig.java
+++ src/main/java/home/picko/springdemobot/config/BotConfig.java
@@ -1,10 +1,20 @@
 package home.picko.springdemobot.config;
 
+import com.google.common.base.Utf8;
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@Slf4j
 @Configuration
 @Data
 @PropertySource("application.properties")
@@ -13,7 +23,16 @@ public class BotConfig {
     @Value("${bot.name}")
     String botName;
 
-    @Value("${bot.token}")
-    String token;
+    String token = "";
 
+    public String getToken() {
+        try {
+            token = Files.readString(Path.of("D:/Java/Projects/Spring-boot-tg-bot/src/main/resources/TOKEN.txt"),
+                    StandardCharsets.UTF_8);
+        }
+        catch (IOException e) {
+            log.error("Error with TOKEN: " + e.getMessage());
+        }
+        return token;
+    }
 }
