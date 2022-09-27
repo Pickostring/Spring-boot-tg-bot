bac254e4a8f0e34b7e7281f135b67e1b05b05aa5
Dmitry Rovinsky <ecco7391@gmail.com>
2022-09-27 22:21:53 +0500

    added project files

---
+++ src/main/java/home/picko/springdemobot/service/TelegramBot.java
@@ -0,0 +1,88 @@
+package home.picko.springdemobot.service;
+
+import home.picko.springdemobot.config.BotConfig;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+
+    final BotConfig config;
+
+    static final String HELP_TEXT = "Help text";
+
+    public TelegramBot(BotConfig config) {
+        this.config = config;
+        List<BotCommand> listOfCommands = new ArrayList<>();
+        listOfCommands.add(new BotCommand("/start", "get welcome text"));
+        listOfCommands.add(new BotCommand("/mydata", "get your data stored"));
+        listOfCommands.add(new BotCommand("/deletemydata", "delete my saved data"));
+        listOfCommands.add(new BotCommand("/info", "how to use this bot"));
+        listOfCommands.add(new BotCommand("/settings", "set your preferences"));
+        try {
+            this.execute(new SetMyCommands(listOfCommands, new BotCommandScopeDefault(), null));
+        }
+        catch (TelegramApiException e){
+            log.error("Error setting bot's command list: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public String getBotUsername() { return config.getBotName(); }
+
+    @Override
+    public String getBotToken() { return config.getToken(); }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+
+            switch (messageText) {
+                case "/start":
+                    startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+                    break;
+                case "/help":
+                    sendMessage(chatId, HELP_TEXT);
+                    break;
+                default: sendMessage(chatId, "Sorry, command was not recognised");
+            }
+        }
+
+    }
+
+    private void startCommandReceived(long chatId, String name) {
+
+        String answer = "Hi, " + name + ", nice to meet you!";
+        log.info("Replied to user " + name);
+
+        sendMessage(chatId, answer);
+
+    }
+
+    private void sendMessage(long chatId, String textToSend) {
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText(textToSend);
+
+        try {
+            execute(message);
+        }
+        catch (TelegramApiException e) {
+            log.error("Error occurred: " + e.getMessage());
+        }
+    }
+}
