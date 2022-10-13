dd044940956b2da58677a04ea7dde418867c307f
Dmitry Rovinsky <ecco7391@gmail.com>
2022-10-13 21:14:39 +0500

    test

--- src/main/java/home/picko/springdemobot/config/BotConfig.java
+++ src/main/java/home/picko/springdemobot/config/BotConfig.java
@@ -1,13 +1,10 @@
 package home.picko.springdemobot.config;
 
-import com.google.common.base.Utf8;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
